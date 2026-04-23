3a39b16eaa94de99ec0aab48d6b0df4e91bc7ddd
JurgenDeLira <arq.jorgedelira@gmail.com>
2026-04-23 16:24:48 -0700

    Inicio proyecto

---
+++ src/test/java/com/batteryplus/sync/mercadopago_sync/MercadopagoSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batteryplus.sync.mercadopago_sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadopagoSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
